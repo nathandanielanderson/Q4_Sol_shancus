66e0a144fa2bb3e24937f9ffd5c41b72b9ad49ec
Nathan Anderson <nathandanielanderson@gmail.com>
2024-11-11 12:27:02 -0600

    switched to devnet in Anchor.toml

--- dp_test_cpi/programs/dp_test_cpi/src/lib.rs
+++ dp_test_cpi/programs/dp_test_cpi/src/lib.rs
@@ -4,13 +4,18 @@ declare_id!("2r9AvbDhiTq9dcAojVmJgnxZX3AH7nsycL6WXtPPqNW4");
 
 #[program]
 pub mod dp_test_cpi {
+    use anchor_lang::solana_program::message;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn log_message(_ctx:Context<LogMessage>, message: String) -> Result<()> {
+        msg!("Unity says: {}", message);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct LogMessage<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
