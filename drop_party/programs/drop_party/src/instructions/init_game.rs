2131f14578411670ddb2129468cc7e7e64dc8241
Nathan Anderson <nathandanielanderson@gmail.com>
2024-11-13 11:55:01 -0600

    refactor deposit, init_game, and init_player instructions; streamline account initialization and structure

--- drop_party/programs/drop_party/src/instructions/init_game.rs
+++ drop_party/programs/drop_party/src/instructions/init_game.rs
@@ -1,21 +1,44 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Token, TokenAccount, Mint, InitializeAccount};
 use crate::state::config::*;
 
 #[derive(Accounts)]
+#[instruction(game_name: String, world_name: String)]
 pub struct InitGame<'info> {
     #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        space = Game::LEN,
+        seeds = [b"game", game_name.as_bytes()],
+        bump,
+    )]
     pub game: Account<'info, Game>,
-    
+    #[account(
+        init,
+        payer = user,
+        space = World::LEN,
+        seeds = [b"world", game.key().as_ref(), world_name.as_bytes()],
+        bump,
+    )]
+    pub world: Account<'info, World>,
+
+    pub system_program: Program<'info, System>,
 }
 
 impl<'info> InitGame<'info> {
-    pub fn initialize_game(&self) -> Result<()> {
-        
-        Ok(())
-    }
-    pub fn initialize_world_0(&self) -> Result<()> {
-        
+    pub fn initialize_game(&mut self, game_name: String, world_name: String) -> Result<()> {
+       
+        self.game.set_inner(Game {
+            authority: *self.user.key,
+            name: game_name,
+        });
+
+        self.world.set_inner(World {
+            authority: self.game.key(),
+            name: world_name,
+        });
+
         Ok(())
     }
 }
--- drop_party/programs/drop_party/src/instructions/init_player.rs
+++ drop_party/programs/drop_party/src/instructions/init_player.rs
@@ -1,17 +1,33 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Token, TokenAccount, Mint, InitializeAccount};
 use crate::state::config::*;
 
 #[derive(Accounts)]
+#[instruction(player_name: String )]
 pub struct InitPlayer<'info> {
     #[account(mut)]
-    player: Account<'info, Player>,
-    
+    pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        space = Player::LEN,
+        seeds = [b"player", player_name.as_bytes()],
+        bump,
+    )]
+    pub player: Account<'info, Player>,
+
+    pub system_program: Program<'info, System>,
 }
 
 impl<'info> InitPlayer<'info> {
-    pub fn initialize_player(&self) -> Result<()> {
-        
+    pub fn initialize_game(&mut self, player_name: String) -> Result<()> {
+       
+        self.player.set_inner(Player {
+            authority: *self.user.key,
+            username: player_name,
+            x_pos: 0,
+            y_pos: 0,
+            z_pos: 0,
+        });
         Ok(())
     }
 }
