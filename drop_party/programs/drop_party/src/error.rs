aeead3fff02de5d4ef4249f9b5e142600e3a1a97
Nathan Anderson <nathandanielanderson@gmail.com>
2024-11-18 12:26:21 -0600

    refactor Deposit instruction; improve error messages, update account handling, and rename variables for clarity

--- drop_party/programs/drop_party/src/error.rs
+++ drop_party/programs/drop_party/src/error.rs
@@ -2,8 +2,8 @@ use anchor_lang::error_code;
 
 #[error_code]
 pub enum ErrorCode {
-    #[msg("Insufficient balance.")]
+    #[msg("The account does not have sufficient balance to complete the operation.")]
     InsufficientBalance,
-    #[msg("Zero balance.")]
+    #[msg("The transfer amount must be greater than zero.")]
     ZeroBalance,
 }
