e13dd9b13a9cfbb7d539e14dc39cd36060063788
Nathan Anderson <nathandanielanderson@gmail.com>
2024-12-02 13:05:12 -0600

    remove wallet and wallet_bump fields from World struct; simplify game state representation

--- drop_party/programs/drop_party/src/state/config.rs
+++ drop_party/programs/drop_party/src/state/config.rs
@@ -15,8 +15,6 @@ pub struct Game {
 #[account]
 pub struct World {
     pub authority: Pubkey,
-    pub wallet: Pubkey,
-    pub wallet_bump: u8,
     pub name: String,
     pub bump: u8,
 }
@@ -42,8 +40,6 @@ impl World {
     pub const LEN: usize = 
     8 +          // discriminator
     PUBKEY_L +   // authority
-    PUBKEY_L +   // wallet
-    1 +          // wallet_bump 
     MAX_NAME_L + // name
     1;           // bump
 }
