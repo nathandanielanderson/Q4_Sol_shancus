b3da35b37ad68a6259bcc36832ca43948584db33
Nathan Anderson <nathandanielanderson@gmail.com>
2024-12-02 13:06:27 -0600

    remove wallet and wallet_bump fields from InitWorld initialization; streamline world setup logic

--- drop_party/programs/drop_party/src/instructions/init_world.rs
+++ drop_party/programs/drop_party/src/instructions/init_world.rs
@@ -20,18 +20,10 @@ pub struct InitWorld<'info> {
 
 impl<'info> InitWorld<'info> {
     pub fn initialize_world(&mut self, world_name: String, bumps: &InitWorldBumps) -> Result<()> {
-       let (wallet, wallet_bump) = Pubkey::find_program_address(
-        &[
-            b"wallet",
-            self.world.key().as_ref(),
-        ],
-        &crate::id(),
-    ); 
+
         self.world.set_inner(World {
             authority: *self.admin.key,
             name: world_name,
-            wallet: wallet,
-            wallet_bump: wallet_bump,
             bump: bumps.world
         });
 
