af2a4d6f5073477720e1f916e45efd415ec8d7e6
Nathan Anderson <nathandanielanderson@gmail.com>
2024-12-02 15:27:38 -0600

    remove unused initialization and deposit functions

--- drop_party/programs/drop_party/src/lib.rs
+++ drop_party/programs/drop_party/src/lib.rs
@@ -14,14 +14,7 @@ declare_id!("37yRf2iJ11DyNyreMCbGxLGhzWSTpr5MRZAJmK8JdCzx");
 pub mod drop_party {
     use super::*;
 
-    // pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-    //     msg!("Greetings from: {:?}", ctx.program_id);
-    //     Ok(())
-    // }
-
-    // pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
-    //     Deposit::deposit_token(ctx, amount)
-    // }
+    //TODO: Add the instructions here
 }
 
 #[derive(Accounts)]
