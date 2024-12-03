494e3769f9c6261b129663a8482b4bf07bb67812
Nathan Anderson <nathandanielanderson@gmail.com>
2024-12-02 18:30:46 -0600

    implement player initialization and logout functions; update world initialization to use context bumps

--- drop_party/programs/drop_party/src/lib.rs
+++ drop_party/programs/drop_party/src/lib.rs
@@ -13,7 +13,17 @@ declare_id!("37yRf2iJ11DyNyreMCbGxLGhzWSTpr5MRZAJmK8JdCzx");
 #[program]
 pub mod drop_party {
     use super::*;
+
     pub fn init_world(ctx: Context<InitWorld>, world_name: String) -> Result<()> {
-        ctx.accounts.initialize_world(world_name, &InitWorldBumps::default())
+        ctx.accounts.initialize_world(world_name, &ctx.bumps)
+    }
+
+    pub fn init_player(ctx: Context<InitPlayer>, player_username: String) -> Result<()> {
+        ctx.accounts.initialize_player(player_username, &ctx.bumps)
+    }
+
+
+    pub fn player_logout(ctx: Context<PlayerLogout>, logout_x_pos: u64, logout_y_pos: u64, logout_z_pos: u64, logout_coins: u64) -> Result<()> {
+        ctx.accounts.player_logout(logout_x_pos, logout_y_pos, logout_z_pos, logout_coins)  
     }
 }
